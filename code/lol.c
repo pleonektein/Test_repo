cfa59081386013271aa147cb00f700293e831d28
peonektein github <roen1012@gmail.com>
2023-03-06 23:53:58 -0600

    This is not usable anymore sadge

--- code/lol.c
+++ code/lol.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("Suicide if you're ever gonna let me know, suicide blah blah blah blah bleh bleh bleh bleh bloh\n");
-    printf("Lulw");
+int main(){
 
-    char letter = a;
-    char assigned_char = b;
-    int  = 32767;
-    num++;
 
-    return 0;
+    
 }
