204c0c017653b32a018d823c189ecb2c606e9bed
peonektein github <roen1012@gmail.com>
2023-03-06 23:24:45 -0600

    llwlwowleowfre

--- code/lol.c
+++ code/lol.c
@@ -4,5 +4,11 @@ int main()
 {
     printf("Suicide if you're ever gonna let me know, suicide blah blah blah blah bleh bleh bleh bleh bloh\n");
     printf("Lulw");
+
+    char letter = a;
+    char assigned_char = b;
+    int  = 32767;
+    num++;
+
     return 0;
 }
